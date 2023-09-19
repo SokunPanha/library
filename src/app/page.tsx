3ccb0d101992c85574d0ea46a99b15ab5503afdf
Raksa952003@#$ <sokunpanha.panha@gmail.com>
2023-09-19 13:09:16 +0700

    final

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,9 @@ export default function Page () {
    <div className="hero min-h-screen animate-dimOverlay" style={{backgroundImage: 'url(https://www.eui.eu/Content-Types-Assets/Web-Unit/Student-in-law-library-section.x7622d211.jpg?w=1920&h=1080)'}}>
   <div className="hero-overlay bg-opacity-60"></div>
   <div className="hero-content text-center text-neutral-content">
+    
     <div className="max-w-md">
+
       <h1 className="mb-5 text-5xl text-white w-full font-bold">Welcome <span className="text-blue-500">{session?.user?.name}</span> to <br />  E-Library</h1>
       <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
       <Link href="/login" ><button className="btn btn-primary animate-bounce" >Get Started</button></Link>
